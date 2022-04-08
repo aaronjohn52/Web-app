77a30bbf8ef3c91960aa1050d13d892ba68b6abd
aaronjohn52 <52210129+aaronjohn52@users.noreply.github.com>
2022-04-09 02:52:31 +0530

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -1,20 +1,22 @@
 import pandas as pd
 df = pd.read_csv(r'C:\Users\aaron\Documents\heart.csv')
 
-from sklearn.ensemble import RandomForestClassifier
+import lightgbm as lgb
+from lightgbm import LGBMClassifier
+
+
 from sklearn.model_selection import train_test_split
 
 X = df.drop('target', axis=1)
 y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-model_rf = RandomForestClassifier()
+model_lgbm = LGBMClassifier()
+model_lgbm.fit(X_train, y_train)
 
-model_rf.fit(X_train,y_train)
-y_pred_rf = model_rf.predict(X_test)
-
+y_pred_lgbm = model_lgbm.predict(X_test)
 
 import pickle
 filename = 'model.pkl'
-pickle.dump(model_rf, open(filename, 'wb'))
+pickle.dump(model_lgbm, open(filename, 'wb'))
 
